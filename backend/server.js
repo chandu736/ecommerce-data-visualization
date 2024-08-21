6a84b64188d8c56e001083151935b308d86446d6
Chandu kousik Ponnaganti <108112556+chandu736@users.noreply.github.com>
2024-08-21 14:54:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,20 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+const cors = require('cors');
+
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 
 // MongoDB connection
